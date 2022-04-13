4825564b049eeea82879583f70adbece19dc987a
doughbryce <31143323+doughbryce@users.noreply.github.com>
2022-04-13 14:26:33 -0700

    problem 6 functional

--- lab.js
+++ lab.js
@@ -89,7 +89,9 @@ for (let i = 0; i < 10; i++) {
   Create a for-loop that runs 10 times, logging the numbers 1 through 10. Ex, on the first iteration it would log 1, on the second iteration it would log 2.
 */
 
-//CODE HERE
+for (let i = 0; i < 10; i++) {
+  console.log(`${i + 1}`);
+}
 
 ////////// PROBLEM 7 //////////
 
