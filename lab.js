ff87fcd3462dff8882067a152b88de4f8031d651
doughbryce <31143323+doughbryce@users.noreply.github.com>
2022-04-13 14:48:30 -0700

    problem 10 functional

--- lab.js
+++ lab.js
@@ -142,7 +142,8 @@ console.log(changeMyMind);
 ////////// PROBLEM 10 //////////
 // Using the "not" operator (!), change the current value of changeMyMind to true (it should currently be set to false due to the if-else statement in Problem 9). After you change the value of changeMyMind, console.log it's new value (it should now read true).
 
-//CODE HERE
+changeMyMind = !changeMyMind;
+console.log(changeMyMind);
 
 ////////// ADVANCED PROBLEMS //////////
 
@@ -151,4 +152,9 @@ console.log(changeMyMind);
 let z = 5;
 // Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
 
-//CODE HERE
+// while (z > 0) {
+//   for (let i = z; i > 0; i -= 1) {
+//     console.log(i);
+//   }
+//   z -= 1;
+// }
